00119083201fd5ce36ecdc025010658a03f16c16
Efen Stakes <efenstakes101@gmail.com>
2019-10-11 10:25:04 +0300

    add application storage variables for network and application storage

---
+++ app/models/net.js
@@ -0,0 +1,4 @@
+// define the application api server url
+export default {
+    SERVER: 'http://slippery-bear-62.localtunnel.me'
+}
---
+++ app/models/storage.js
@@ -0,0 +1,16 @@
+export default {
+
+    IS_FIRST_VISIT: 'klm:go:is-first-visit',
+
+    USER: 'klm:go:user',
+    TOKEN: 'klm:go:user:token',
+    TOKEN_TIME: 'klm:go:user:token:offer-time',
+    FAQS: 'klm:go:faqs',
+    FAQS_LAST_FETCH: 'klm:go:faqs:last-fetch',
+    INSTRUCTIONS: 'klm:go:instructions',
+    INSTRUCTIONS_LAST_FETCH: 'klm:go:instructions:last-fetch',
+
+    FAQ_LAST_RATED: 'klm:go:faqs:last-rated',
+    INSTRUCTION_LAST_RATED: 'klm:go:instructions:last-rated', 
+
+}
